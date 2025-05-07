0d0b2022054cecd2be211b300fd7840c543caff6
esewi osemwegie <kulboy900@gmail.com>
2025-05-07 20:08:26 +0200

    modificato condizione for

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ function fizzBuzz(n) {
 
     for (let i = 1; i <= n; i++) {
        
-        if( i % 3 == 0){
+        if( i % 15 == 0){
             console.log(`Buzz`); 
 
         }else if( i % 5 == 0){
             console.log(`Fizz`);
             
-        }else if(i % 15 == 0 ){
-            console.log(`FizzBuzz`);
+        }else if(i % 3 == 0 ){
+            console.log(`Buzz`);
             
         }else{
             console.log(i);
